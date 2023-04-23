105a16b418c0df0c1a08d6caa9b4c49fd08239cd
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-04-22 18:48:38 -0700

    initialized arrays and created print functions

--- Dijkstra Algorithm/Program.cs
+++ Dijkstra Algorithm/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Taylor!!!!!");
+            int[] distance = { 0, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue };
+            PrintDistArr(distance);
+
+            char[] unvisitedNodes = { 'A', 'B', 'C', 'D', 'E' };
+            PrintUnvisitedArr(unvisitedNodes);
+
+            char[] visitedNodes = new char[unvisitedNodes.Length];
+            PrintVisitedArr(visitedNodes);
+
+            while (unvisitedNodes != null)  // keep iterating
+            {
+                int count = 1;
+                Console.WriteLine($"---------- Iteration Number {count} ----------");
+                break;
+
+
+            }
+
+        }
+
+        public static void PrintDistArr(int[] array)
+        {
+            Console.WriteLine("Distance array contains:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "  ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static void PrintUnvisitedArr(char[] array)
+        {
+            Console.WriteLine("Unvisited nodes array contains:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "  ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static void PrintVisitedArr(char[] array)
+        {
+            Console.WriteLine("Visited nodes array contains:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "  ");
+            }
+            Console.WriteLine("\n");
         }
     }
 }
